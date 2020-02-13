b7bd1025bd24a0c37e4b3d94c49a0e1c529f817e
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-02-13 15:59:09 +0000

    updates

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const cheerioReq = require("cheerio-req");
+const { Parser } = require('json2csv');
+
+const fields = [
+  'job_family',
+  'role',
+  'role_level',
+  'role_description_intro',
+  'skills_they_need',
+  'skill_name',
+  'skill_description'
+];
+const opts = { fields };
 
 cheerioReq("https://www.gov.uk/guidance/software-developer", (err, $) => {
-    let data = {
-      role_title: $("h1").text().trim(),
-      role_levels: $('.govspeak h2')
-        .map( (index, role) => {
+    let role = {
+      family: "Technical job",
+      title: $("h1").text().trim(),
+      levels: $('.govspeak h2')
+        .map( (index, level) => {
           return {
-            title: $(role).text().trim(),
-            description: $(role).next('p').text().split('.')[0] + ".",
-            duties: $(role).next('p').next('ul').find('li').map( (i, duty) => {
+            title: $(level).text().trim(),
+            description: $(level).next('p').text().split('.')[0] + ".",
+            duties: $(level).next('p').next('ul').find('li').map( (i, duty) => {
               return $(duty).text().trim();
             }).toArray(),
-            skills: $(role).next('p').next('ul').next('h3').next('ul').find('li').map( (i, duty) => {
+            skills: $(level).next('p').next('ul').next('h3').next('ul').find('li').map( (i, duty) => {
               return {
                 name: $(duty).find('strong').text().trim(),
                 description: $(duty).text().trim()
@@ -20,8 +33,32 @@ cheerioReq("https://www.gov.uk/guidance/software-developer", (err, $) => {
           }
         })
         .toArray()
-        .slice(1, -1)
+        //.slice(1, -1)
     };
-    console.log(JSON.stringify(data, null, 2));
+    //console.log(JSON.stringify(role, null, 2));
+    //console.log(JSON.stringify(formatForCSV(role), null, 2));
+
+    const json2csvParser = new Parser({ fields });
+    const csv = json2csvParser.parse(formatForCSV(role));
+    console.log(csv);
     //console.log(data);
 });
+
+
+function formatForCSV(role) {
+  var data = [];
+  role.levels.forEach( level => {
+    level.skills.forEach( skill => {
+      data.push({
+        job_family: role.family,
+        role: role.title,
+        role_level: level.title,
+        role_description_intro: role.levels[0].description,
+        skills_they_need: level.description,
+        skill_name: skill.name,
+        skill_description: skill.description
+      });
+    });
+  });
+  return data;
+}
